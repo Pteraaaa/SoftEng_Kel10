0bf4ca0d582e3c0f3dfa5c556eba29e56ef5167e
Pteraaaaz <barsenrio@gmail.com>
2026-03-19 12:49:59 +0700

    Auth Screen 6

--- myapp/lib/Widgets/LoginForm.dart
+++ myapp/lib/Widgets/LoginForm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/Screens/HomeScreen.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -9,51 +10,76 @@ class LoginForm extends StatefulWidget {
 
 class _LoginFormState extends State<LoginForm> {
   bool isHidden = true;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text("Email Address"),
-        const SizedBox(height: 6),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "yourname@example.com",
-            prefixIcon: Icon(Icons.email),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Email Address"),
+          const SizedBox(height: 6),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "yourname@example.com",
+              prefixIcon: Icon(Icons.email),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Email is required";
+              }
+              if (!value.contains("@")) {
+                return "Invalid Email";
+              }
+            },
           ),
-        ),
-        const SizedBox(height: 20),
+          const SizedBox(height: 20),
 
-        const Text("Password"),
-        const SizedBox(height: 6),
-        TextField(
-          obscureText: isHidden,
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.lock),
-            hintText: "Enter your password",
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-            suffixIcon: IconButton(
-              onPressed: () {
-                setState(() {
-                  isHidden = !isHidden;
-                });
-              },
-              icon: Icon(isHidden ? Icons.visibility : Icons.visibility_off),
+          const Text("Password"),
+          const SizedBox(height: 6),
+          TextField(
+            obscureText: isHidden,
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock),
+              hintText: "Enter your password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    isHidden = !isHidden;
+                  });
+                },
+                icon: Icon(isHidden ? Icons.visibility : Icons.visibility_off),
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 20),
+          const SizedBox(height: 20),
 
-        SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
-            onPressed: () {},
-            child: const Text("Login"),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  print("Valid");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const HomeScreen()),
+                  );
+                }
+              },
+              child: const Text("Login"),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
